29a638036e1e044e65f7f3b80af3d91cdfc5ffd5
rishprab <40069626+rishprab@users.noreply.github.com>
2021-05-17 16:53:12 +1000

    Add files via upload

---
+++ src/java/Uts/isd/model/dao/StaffDAO.java
@@ -0,0 +1,101 @@
+package uts.isd.model.dao;
+
+import uts.isd.model.Staff;
+
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+
+public class StaffDAO {
+    public static Staff get(String email, String password) throws SQLException, DAOException {
+        String getStaffQuery =
+                "SELECT * FROM ACCOUNTS A " +
+                "INNER JOIN STAFF S on A.ID = S.ID " +
+                "WHERE EMAIL LIKE ? AND PASSWORD LIKE ?";
+
+        PreparedStatement st = DAOUtils.prepareStatement(getStaffQuery, false, email, password);
+        ResultSet staffRs = st.executeQuery();
+
+        if (!staffRs.next())
+            throw new DAOException("No Staff found. Incorrect Email or Password.");
+
+        return createStaffObject(staffRs);
+    }
+
+    public static Staff get(int accountID) throws SQLException, DAOException {
+        String getStaffDataQuery =
+                "SELECT * FROM ACCOUNTS A " +
+                "INNER JOIN STAFF S on A.ID = S.ID " +
+                "WHERE A.ID = ?";
+
+        PreparedStatement st = DAOUtils.prepareStatement(getStaffDataQuery, false, accountID);
+        ResultSet staffRs = st.executeQuery();
+
+        if (!staffRs.next())
+            throw new DAOException("No Staff with that ID exists.");
+
+        return createStaffObject(staffRs);
+    }
+
+    public static List<Staff> getAll() throws SQLException {
+        LinkedList<Staff> customers = new LinkedList<>();
+
+        String getUserIdQuery =
+                "SELECT * FROM ACCOUNTS A " +
+                "INNER JOIN STAFF S on A.ID = S.ID";
+
+        PreparedStatement st = DAOUtils.prepareStatement(getUserIdQuery, false);
+        ResultSet customersRs = st.executeQuery();
+
+        while (customersRs.next()) {
+            customers.add(createStaffObject(customersRs));
+        }
+
+        return customers;
+    }
+
+    public static void save(Staff staff) throws SQLException, DAOException {
+        int newID = AccountDAO.save(staff);
+
+        String staffInsertQuery = "INSERT INTO STAFF (ID, IS_ADMIN) VALUES (?, ?)";
+        PreparedStatement staffInsertSt = DAOUtils.prepareStatement(staffInsertQuery, false, newID, staff.isAdmin());
+
+        int rowsChanged = staffInsertSt.executeUpdate();
+        if (rowsChanged == 0)
+            throw new DAOException("Failed to create staff profile. Please try again.");
+    }
+
+    /**
+     * Update a single account from the database.
+     *
+     * @param staff The instantiated account to update. Must have an ID.
+     */
+    public static void update(Staff staff) throws SQLException, DAOException {
+        AccountDAO.update(staff);
+
+        String updateQuery = "UPDATE STAFF SET IS_ADMIN = ? WHERE ID = ?";
+        PreparedStatement updateSt = DAOUtils.prepareStatement(updateQuery, false,
+                staff.isAdmin(),
+                staff.getID()
+        );
+
+        int rowsChanged = updateSt.executeUpdate();
+        if (rowsChanged == 0)
+            throw new DAOException("Failed to update Staff details. Please try again.");
+    }
+
+    // Helpers
+    private static Staff createStaffObject(ResultSet staffRs) throws SQLException {
+        Staff staff = new Staff();
+        staff.setID(staffRs.getInt("ID"));
+        staff.setFirstName(staffRs.getString("F_NAME"));
+        staff.setLastName(staffRs.getString("L_NAME"));
+        staff.setEmail(staffRs.getString("EMAIL"));
+        staff.setPassword(staffRs.getString("PASSWORD"));
+        staff.setContactNumber(staffRs.getString("CONTACT_NUMBER"));
+        staff.setActive(staffRs.getBoolean("IS_ACTIVE"));
+        staff.setAdmin(staffRs.getBoolean("IS_ADMIN"));
+
+        return staff;
+    }
+}
---
+++ src/java/Uts/isd/model/dao/UserAccessDAO.java
@@ -0,0 +1,95 @@
+
+package uts.isd.model.dao;
+
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+import uts.isd.model.Log;
+import java.sql.Timestamp;
+
+public class UserAccessDAO {
+    
+    public static List<Log> getLogs(int accountID) throws SQLException {
+        LinkedList<Log> logs = new LinkedList<>();
+        
+        String getUserLogs = 
+                "SELECT * FROM USER_ACCESS WHERE (ACCOUNT_ID = ?)";
+        
+        PreparedStatement getUserLog = DAOUtils.prepareStatement(
+                getUserLogs, 
+                true, 
+                accountID);
+        ResultSet logRs = getUserLog.executeQuery();
+        
+        while (logRs.next()) {
+            logs.add(createLogsObject(logRs));
+        }
+        return logs;
+       
+        }
+    
+    public static int save(int account_id, String action) throws SQLException, DAOException 
+    {
+     String query =
+                "INSERT INTO USER_ACCESS (ACCOUNT_ID, PERFORMED_ON, ACTION)" +
+                "VALUES (?, CURRENT_TIMESTAMP, ?)";
+     
+     PreparedStatement accessInsertSt = DAOUtils.prepareStatement(
+             query, 
+             true,
+             account_id,
+             action             
+     );
+     
+     int rowsChanged = accessInsertSt.executeUpdate();
+     if (rowsChanged == 0)
+         throw new DAOException("User access creation failed. Please try again.");
+     
+     return DAOUtils.getGeneratedID(accessInsertSt);
+    }
+    
+    public static List<Log> getDate(int account_id, String date) throws SQLException
+    {
+        LinkedList<Log> logs = new LinkedList<>();
+        
+        Timestamp startDate = Timestamp.valueOf(date+ " 00:00:00");
+        Timestamp endDate = Timestamp.valueOf(date + " 23:59:59");
+        
+        String dateTimeQuery = 
+                "SELECT * "
+                + "FROM USER_ACCESS "
+                + "WHERE ACCOUNT_ID = ? "
+                + "AND PERFORMED_ON >= '" 
+                + startDate.toString()
+                + "' AND PERFORMED_ON < '"
+                + endDate.toString()
+                +"'"
+                ; 
+        
+        PreparedStatement getDate = DAOUtils.prepareStatement(
+                dateTimeQuery,
+                false,
+                account_id 
+        );
+        
+        ResultSet dateRs = getDate.executeQuery();
+        
+        while (dateRs.next()) {
+            logs.add(createLogsObject(dateRs));
+        }
+        
+        return logs;
+        
+    }
+    
+        //Helper
+    private static Log createLogsObject(ResultSet logRs) throws SQLException {
+        Log logs = new Log();
+        logs.setID(logRs.getInt("ACCOUNT_ID"));
+        logs.setPerformedOn(logRs.getString("PERFORMED_ON"));
+        logs.setAction(logRs.getString("ACTION"));
+        return logs;
+        
+    }
+
+    }
